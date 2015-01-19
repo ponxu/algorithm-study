9a12251fa68370a0111de2f40b13a4a9826e6fc4
ponxu <xwz689@gmail.com>
2015-01-19 10:45:25 +0800

    selection sort

--- src/com/ponxu/test/algorithm/TestSelectionSort.java
+++ src/com/ponxu/test/algorithm/TestSelectionSort.java
@@ -1,7 +1,31 @@
 package com.ponxu.test.algorithm;
 
+import java.util.Arrays;
+
 /**
  * Created by vince on 15-1-19.
  */
 public class TestSelectionSort {
+    public static void selectionSort(int[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[minPos])
+                    minPos = j;
+            }
+
+            if (minPos != i) {
+                int temp = a[i];
+                a[i] = a[minPos];
+                a[minPos] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = Utils.randomIntArray(5, 10);
+        System.out.println("I: " + Arrays.toString(a));
+        selectionSort(a, a.length);
+        System.out.println("O: " + Arrays.toString(a));
+    }
 }
